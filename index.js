e77849084ae8c16c8dcced4a5525f1daa489957b
Елена Карпова <karpova.el.m@gmail.com>
2024-06-21 00:17:18 +0200

    Working posts + posts/id

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import path from "path";
-// import { requstTime } from "./middlewares.js";
-import { createRequire } from "module";
+import {createRequire} from "module";
 const require = createRequire(import.meta.url);
 
 const app = express();
@@ -10,31 +9,10 @@ const __dirname = path.resolve();
 const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database("yourdatabase.db");
 
-db.run(
-  `CREATE TABLE IF NOT EXISTS POSTS (
-        postId INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-        authorName TEXT(20) NOT NULL,
-        title TEXT(250) NOT NULL,
-        location TEXT(20) NOT NULL,
-        publicationDate DATETIME NOT NULL)`
-);
-
-// app.use(requstTime);
-app.use(express.json());
 app.set("view engine", "ejs");
 app.set("views", path.resolve(__dirname, "templates"));
 
-app.get("/", (req, res) => {
-  res.render("write_post", { title: "Write post", active: "write_post" });
-});
-
-app.get("/posts", (req, res) => {
-  res.render("posts", { title: "Posts", active: "posts" });
-});
-
-app.get("/about", (req, res) => {
-  res.render("about", { title: "About Blog", active: "about" });
-});
+app.use(express.json());
 
 async function startApp() {
   try {
@@ -47,18 +25,93 @@ async function startApp() {
 }
 startApp();
 
+db.run(
+  `CREATE TABLE IF NOT EXISTS POSTS (
+        postId INTEGER PRIMARY KEY AUTOINCREMENT,
+        authorName TEXT(20),
+        title TEXT(250),
+        location TEXT(20),
+        publicationDate DATETIME DEFAULT CURRENT_TIMESTAMP)`
+);
+
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About Blog", active: "about" });
+});
+
+app.get("/", (req, res) => {
+  res.render("writePost", { title: "Write post", active: "writePost" })
+});
+
 app.get("/posts/:id", (req, res, next) => {
-    var params = [req.params.id]
-    db.get(`SELECT * FROM posts where postId = ?`, [params], (err, row) => {
+    db.all(`SELECT * FROM posts where postId = ?`, [req.params.id], (err, row) => {
         if (err) {
-          res.status(400).json({"error": err.message});
+          res.render("post", { title: err.message, active: "post" });
           return next();
         }
         if (!row || !row.length) {
-          res.status(400).json({"error": "User not found"});
+          res.render("postNotFound", { title: "Post not found", active: "post" });
           return next();
         }
-        res.status(200).json(row);
+        res.render("post", { title: row.postId, post: row, active: "post" });
       });
 });
 
+app.get("/posts", (req, res, next) => {
+  db.all("SELECT * FROM posts", [], (err, rows) => {
+      if (err) {
+        res.render("posts", { title: err.message, active: "posts" });
+        return next();
+      }
+      if (!rows || !rows.length) {
+        res.render("posts", { title: "Posts not found", active: "posts" });
+        return next();
+      }
+      res.render("posts", { title: "Posts", titleBd: rows, active: "posts" });
+    });
+});
+
+app.post("/", (req, res, next) => {
+  var reqBody = req.body;
+  db.run(`INSERT INTO posts (authorName, title, location) VALUES (?,?,?)`,
+      [reqBody.authorName, reqBody.title, reqBody.location, reqBody.publicationDate],
+      function (err, result) {
+        if (err) {
+          res.render("/", { title: err.message, active: "post" });
+          return next();
+        }
+        let title = this.lastID
+        res.status(201).json({
+            "post_id": title
+        })
+        res.render("writePost", { title: title, active: "writePost" });
+    });
+});
+
+app.put("/posts/:id", (req, res, next) => {
+  var reqBody = req.body;
+  db.all(`UPDATE posts SET authorName = ?, title = ?, location = ? WHERE postId = ?`,
+      [reqBody.authorName, reqBody.title, reqBody.location, req.params.id],
+      function (err, result) {
+          if (err) {
+              res.status(400).json({ "error": res.message })
+              return next();
+          }
+          res.status(200).json(this.changes);
+      });
+
+});
+
+app.delete("/posts/delete/:id", (req, res, next) => {
+  db.all(`DELETE FROM posts WHERE postId = ?`, [req.params.id], function (err, result) {
+          if (err) {
+              res.status(400).json({ "error": res.message })
+              return next();
+          }
+          if (this.changes > 0) {
+            res.render("delete", { title: "Post has been deleted", active: "post", result: this.changes });
+            res.status(200).json(this.changes)
+          } else {
+            res.render("postNotFound", { title: "Posts not found", active: "post" });
+          }
+      });
+});
--- middlewares.js
+++
@@ -1,4 +0,0 @@
-export function requstTime(req, res, next) {
-    req.requstTime = Date.now()
-    next()
-}
