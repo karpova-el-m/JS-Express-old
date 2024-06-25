22880d053d9dc3f39fd95198b2c8e98f07fcfdf4
Елена Карпова <karpova.el.m@gmail.com>
2024-06-25 21:28:41 +0200

    Formatting

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
+// http://localhost:3000/posts
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+require('dotenv').config();
+const env = process.env
+
 import express from "express";
 import path from "path";
-import {createRequire} from "module";
-const require = createRequire(import.meta.url);
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 const __dirname = path.resolve();
-const sqlite3 = require("sqlite3").verbose();
-const db = new sqlite3.Database("yourdatabase.db");
+
+const sqlite3 = require(env.SQLITE3).verbose();
+const db = new sqlite3.Database(env.DB_NAME);
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve(__dirname, "templates"));
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
 
 async function startApp() {
   try {
@@ -40,35 +45,35 @@ app.get("/about", (req, res) => {
 });
 
 app.get("/", (req, res) => {
-  res.render("startPage", { title: "Write post", active: "-" })
+  res.render("startPage", { title: "Write post", active: "-" });
 });
 
 app.get("/posts/:id", (req, res, next) => {
-    db.all(`SELECT * FROM posts where postId = ?`, [req.params.id], (err, row) => {
-        if (err) {
-          res.render("post", { title: err.message, active: "-" });
-          return next();
-        }
-        if (!row || !row.length) {
-          res.render("postNotFound", { title: "Post not found", active: "-" });
-          return next();
-        }
-        res.render("post", { title: row.postId, post: row, active: "-" });
-      });
+  db.all(
+    `SELECT * FROM posts where postId = ?`,
+    [req.params.id],
+    (err, row) => {
+      if (err) {
+        res.render("post", { title: err.message, active: "-" });
+      }
+      if (!row || !row.length) {
+        res.render("postNotFound", { title: "Post not found", active: "-" });
+      }
+      res.render("post", { title: row.postId, post: row, active: "-" });
+    }
+  );
 });
 
 app.get("/posts", (req, res, next) => {
   db.all("SELECT * FROM posts", [], (err, rows) => {
-      if (err) {
-        res.render("posts", { title: err.message, active: "posts" });
-        return next();
-      }
-      if (!rows || !rows.length) {
-        res.render("postNotFound", { title: "Posts not found", active: "posts" });
-        return next();
-      }
-      res.render("posts", { title: "Posts", titleBd: rows, active: "posts" });
-    });
+    if (err) {
+      res.render("posts", { title: err.message, active: "posts" });
+    }
+    if (!rows || !rows.length) {
+      res.render("postNotFound", { title: "Posts not found", active: "posts" });
+    }
+    res.render("posts", { title: "Posts", titleBd: rows, active: "posts" });
+  });
 });
 
 app.get("/write_post", (req, res) => {
@@ -76,45 +81,63 @@ app.get("/write_post", (req, res) => {
 });
 
 app.post("/write_post/saved", (req, res, next) => {
-  var reqBody = {...req.body};
-  db.run(`INSERT INTO posts (authorName, title, location) VALUES (?,?,?)`,
-      [reqBody.authorName, reqBody.title, reqBody.location, reqBody.publicationDate],
-      function (err, result) {
-        if (err) {
-          res.render("writePost", { title: err.message, active: "-" });
-          return next();
-        }
-        let title = `Your post has been saved, post ID: ${this.lastID}`
-        res.render("writePost", { title: title, active: "writePost" });
-    });
+  var reqBody = { ...req.body };
+  db.run(
+    `INSERT INTO posts (authorName, title, location) VALUES (?,?,?)`,
+    [
+      reqBody.authorName,
+      reqBody.title,
+      reqBody.location,
+      reqBody.publicationDate,
+    ],
+    function (err, result) {
+      if (err) {
+        res.render("writePost", { title: err.message, active: "-" });
+      }
+      let title = `Your post has been saved, post ID: ${this.lastID}`;
+      res.render("writePost", { title: title, active: "writePost" });
+    }
+  );
 });
 
 app.get("/posts/update/:id", (req, res) => {
-  const id = req.params.id
-  res.render("updatePost", { title: `Update post, post ID: ${id}`, id: id, active: "-" });
+  const id = req.params.id;
+  res.render("updatePost", {
+    title: `Update post, post ID: ${id}`,
+    id: id,
+    active: "-",
+  });
 });
 
 app.get("/updated/:id", (req, res, next) => {
-  var reqBody = {...req.query};
-  db.all(`UPDATE posts SET authorName = ?, title = ?, location = ? WHERE postId = ?`,
-      [reqBody.authorName, reqBody.title, reqBody.location, req.params.id],
-      function (err, result) {
-          if (err) {
-              res.status(400).json({ "error": res.message })
-              return next();
-          }
-          let title = `Your post has been updated, ID: ${req.params.id}`
-          res.render("result", { title: title, active: "-" });
-      });
+  var reqBody = { ...req.query };
+  db.all(
+    `UPDATE posts SET authorName = ?, title = ?, location = ? WHERE postId = ?`,
+    [reqBody.authorName, reqBody.title, reqBody.location, req.params.id],
+    function (err, result) {
+      if (err) {
+        res.status(400).json({ error: res.message });
+      }
+      let title = `Your post has been updated, ID: ${req.params.id}`;
+      res.render("result", { title: title, active: "-" });
+    }
+  );
 });
 
 app.get("/posts/delete/:id", (req, res, next) => {
-  db.all(`DELETE FROM posts WHERE postId = ?`, [req.params.id], function (err, result) {
-          if (err) {
-              res.status(400).json({ "error": res.message })
-              return next();
-          } else {
-          res.render("result", { title: "Post has been deleted", active: "post", result: this.changes });
-          } 
-      });
+  db.all(
+    `DELETE FROM posts WHERE postId = ?`,
+    [req.params.id],
+    function (err, result) {
+      if (err) {
+        res.status(400).json({ error: res.message });
+      } else {
+        res.render("result", {
+          title: "Post has been deleted",
+          active: "post",
+          result: this.changes,
+        });
+      }
+    }
+  );
 });
